28a6bad5b50a45cc30b543b511dc0717b31061c1
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-17 10:45:09 -0500

    feat: remove unused import in DistritoController for cleaner code

--- src/main/java/com/cavoshcoffee/backend/controller/DistritoController.java
+++ src/main/java/com/cavoshcoffee/backend/controller/DistritoController.java
@@ -4,7 +4,6 @@ import com.cavoshcoffee.backend.config.Constant;
 import com.cavoshcoffee.backend.dto.GlobalResponse;
 import com.cavoshcoffee.backend.service.DistritoService;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
