d983e027b67bd30965d7da51447d2c124b6cbd53
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-17 11:43:16 -0500

    feat: add ProductoRequestDTO and ProductoMapper for product entity mapping

---
+++ src/main/java/com/cavoshcoffee/backend/dto/request/ProductoRequestDTO.java
@@ -0,0 +1,17 @@
+package com.cavoshcoffee.backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductoRequestDTO {
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private String detalle;
+    private String categoria;
+    private String imagen;
+}
---
+++ src/main/java/com/cavoshcoffee/backend/mapper/ProductoMapper.java
@@ -0,0 +1,23 @@
+package com.cavoshcoffee.backend.mapper;
+
+import com.cavoshcoffee.backend.dto.request.ProductoRequestDTO;
+import com.cavoshcoffee.backend.entity.Producto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductoMapper {
+    public static Producto toEntity(ProductoRequestDTO producto) {
+        if (producto == null) {
+            return null;
+        }
+
+        Producto newProducto = new Producto();
+        newProducto.setNombre(producto.getNombre());
+        newProducto.setDescripcion(producto.getDescripcion());
+        newProducto.setPrecio(producto.getPrecio());
+        newProducto.setDetalle(producto.getDetalle());
+        newProducto.setCategoria(producto.getCategoria());
+        newProducto.setImagen(producto.getImagen());
+        return newProducto;
+    }
+}
