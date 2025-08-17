88b71f672610fd0595b65f8764b0c70a50cce01b
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-17 11:15:28 -0500

    feat: remove unused message constants and related methods from Constant.java for cleaner code

--- src/main/java/com/cavoshcoffee/backend/config/Constant.java
+++ src/main/java/com/cavoshcoffee/backend/config/Constant.java
@@ -9,29 +9,7 @@ public class Constant {
     public static final String GR_ERROR_PARAMETER_TYPE = "The parameter '%s' must be of type '%s'";
     public static final String GR_ERROR_DETAILS = "[%s] %s";
 
-    // Messages for all entities
-    public static final String ALL_RETRIEVED = "All %s retrieved";
-    public static final String RETRIEVED_ATTRIBUTE = "%s %s - %s: %s";
-    public static final String GENERAL_ERROR = "Error retrieving %s";
-
     // Table messages
     public static final String DISTRICT_TABLE = "distrito";
     public static final String PRODUCT_TABLE = "producto";
-
-    // Specific messages for entities
-    public static String str_allRetrieved(String table) {
-        return String.format(ALL_RETRIEVED, table);
-    }
-
-    public static String str_retrieved(String table, Long id) {
-        return String.format(RETRIEVED_ATTRIBUTE, table, "retrieved", "id", id);
-    }
-
-    public static String str_generalError(String table) {
-        return String.format(GENERAL_ERROR, table);
-    }
-
-    public static String str_notFound(String table, Long id) {
-        return String.format(RETRIEVED_ATTRIBUTE, table, "not found", "id", id);
-    }
 }
