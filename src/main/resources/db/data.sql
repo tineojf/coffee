e68fe0770c1a768aeb7638bb3a7585f46b7cc176
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-17 11:36:09 -0500

    feat: add initial product entries to data.sql for café and bakery items

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -17,3 +17,30 @@ insert into distrito (nombre) values
 ('Villa El Salvador'),
 ('San Juan de Lurigancho'),
 ('Comas');
+
+-- insert producto
+insert into producto (nombre, descripcion, precio, detalle, categoria, imagen, esNuevo) values
+-- Cafés
+('Café Americano', 'Café filtrado estilo americano', 6.50, 'Taza 8oz', 'Café', 'https://www.shutterstock.com/image-photo/coffee-cup-saucer-teaspoon-pack-600nw-2337186769.jpg', 1),
+('Espresso', 'Café concentrado servido en taza pequeña', 5.00, 'Shot simple', 'Café', 'https://www.shutterstock.com/image-photo/coffee-cup-saucer-teaspoon-pack-600nw-2337186769.jpg', 0),
+('Cappuccino', 'Café espresso con leche vaporizada y espuma', 8.50, 'Taza 12oz', 'Café', 'https://www.shutterstock.com/image-photo/coffee-cup-saucer-teaspoon-pack-600nw-2337186769.jpg', 1),
+('Latte', 'Café espresso con leche cremosa', 8.00, 'Taza 12oz', 'Café', 'https://www.shutterstock.com/image-photo/coffee-cup-saucer-teaspoon-pack-600nw-2337186769.jpg', 0),
+('Mocha', 'Latte con chocolate y crema batida', 9.50, 'Taza 12oz', 'Café', 'https://www.shutterstock.com/image-photo/coffee-cup-saucer-teaspoon-pack-600nw-2337186769.jpg', 0),
+
+-- Fríos
+('Frappé Vainilla', 'Bebida fría a base de café y vainilla', 11.00, 'Vaso 16oz', 'Bebida fría', 'https://img.freepik.com/psd-gratis/refrescante-bebida-cola-fria-vaso-salpicaduras_632498-25634.jpg', 1),
+('Iced Latte', 'Café espresso con leche y hielo', 9.00, 'Vaso 16oz', 'Bebida fría', 'https://img.freepik.com/psd-gratis/refrescante-bebida-cola-fria-vaso-salpicaduras_632498-25634.jpg', 0),
+
+-- Pastelería
+('Croissant', 'Hojaldre de mantequilla horneado', 4.50, 'Unidad', 'Pastelería', 'https://www.shutterstock.com/image-photo/assorted-fancy-cakes-shopwindow-260nw-2189877387.jpg', 0),
+('Muffin de Arándanos', 'Muffin suave con trozos de arándanos', 5.00, 'Unidad', 'Pastelería', 'https://www.shutterstock.com/image-photo/assorted-fancy-cakes-shopwindow-260nw-2189877387.jpg', 1),
+('Brownie', 'Brownie de chocolate con nueces', 6.00, 'Porción', 'Pastelería', 'https://www.shutterstock.com/image-photo/assorted-fancy-cakes-shopwindow-260nw-2189877387.jpg', 0),
+
+-- Sándwiches
+('Sándwich de Jamón y Queso', 'Pan artesanal con jamón y queso fundido', 10.00, 'Unidad', 'Sándwich', 'https://bimbousa.com/sites/default/files/2022-07/Receta-Sandwich-de-pollo-a-la-mostaza-Bimbo.jpg', 0),
+('Sándwich de Pollo', 'Pan ciabatta con pollo a la parrilla y lechuga', 12.00, 'Unidad', 'Sándwich', 'https://bimbousa.com/sites/default/files/2022-07/Receta-Sandwich-de-pollo-a-la-mostaza-Bimbo.jpg', 1),
+
+-- Extras
+('Té Verde', 'Infusión de té verde natural', 7.00, 'Taza 8oz', 'Infusiones', 'https://static.vecteezy.com/system/resources/thumbnails/040/174/391/small/ai-generated-pictures-of-delicious-and-beautiful-drinks-photo.jpg', 0),
+('Limonada Frozen', 'Refrescante limonada frappé', 8.50, 'Vaso 16oz', 'Bebida fría', 'https://static.vecteezy.com/system/resources/thumbnails/040/174/391/small/ai-generated-pictures-of-delicious-and-beautiful-drinks-photo.jpg', 1),
+('Agua Mineral', 'Botella de agua sin gas', 3.50, '500ml', 'Otros', 'https://static.vecteezy.com/system/resources/thumbnails/040/174/391/small/ai-generated-pictures-of-delicious-and-beautiful-drinks-photo.jpg', 0);
