78705723b544a16f148d7570f4d07bf5e24b940d
deku <joaourteaga52@gmail.com>
2025-08-16 22:13:08 -0500

    Adding the initial structure

---
+++ src/main/java/com/cavoshcoffee/backend/CavoshCoffeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cavoshcoffee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CavoshCoffeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CavoshCoffeeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cavoshcoffee/backend/CavoshCoffeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavoshcoffee.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavoshCoffeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
