1948fc46b7507e3b41298f180a42fd18d13cc936
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-17 10:28:55 -0500

    feat: add initial distrito data insertion to data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,19 @@
 insert into usuario(nombres,correo, passwordd) values ('admin','admin@mail.com','admin123');
+
+-- insert distrito
+insert into distrito (nombre) values
+('Miraflores'),
+('San Isidro'),
+('San Borja'),
+('Santiago de Surco'),
+('La Molina'),
+('San Miguel'),
+('Magdalena del Mar'),
+('Pueblo Libre'),
+('Jesús María'),
+('Lince'),
+('Barranco'),
+('Chorrillos'),
+('Villa El Salvador'),
+('San Juan de Lurigancho'),
+('Comas');
