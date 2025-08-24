55ad34f77c8077121b85ebc3a91a0ee2500a1279
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-24 10:26:23 -0500

    feat: enable h2 with spring-security

--- src/main/java/com/cavoshcoffee/backend/security/SecurityConfig.java
+++ src/main/java/com/cavoshcoffee/backend/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cavoshcoffee.backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
